b2047bec0bbec8bb601f65645cd145ec731c0745
David_Win <997004866@qq.com>
2018-03-31 13:07:01 +0800

    windows测试提交，并新增userController

---
+++ src/main/java/com/turtlebot/operation/web/UserController.java
@@ -0,0 +1,28 @@
+package com.turtlebot.operation.web;
+
+
+import com.turtlebot.operation.dataobject.User;
+import com.turtlebot.operation.service.user.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Resource
+    private UserService userService;
+
+    @RequestMapping(value = "/getUserList",method = RequestMethod.GET)
+    public String getUserList(ModelMap modelMap){
+        List<User> userList=userService.getUserList();
+        modelMap.put("UserList",userList);
+        return "UserList";
+    }
+
+}
