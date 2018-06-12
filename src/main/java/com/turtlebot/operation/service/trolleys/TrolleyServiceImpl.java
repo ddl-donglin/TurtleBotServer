0f7aa942b51c1b01085a19e2da9fdb3ded2b3cef
develop-ddl <997004866@qq.com>
2018-06-12 16:47:31 +0800

    写了个假数据

--- src/main/java/com/turtlebot/operation/service/trolleys/TrolleyServiceImpl.java
+++ src/main/java/com/turtlebot/operation/service/trolleys/TrolleyServiceImpl.java
@@ -1,6 +1,7 @@
 package com.turtlebot.operation.service.trolleys;
 
 import com.turtlebot.operation.dataobject.Indent;
+import com.turtlebot.operation.utils.SSHshell;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,10 +12,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class TrolleyServiceImpl implements TrolleyService {
 
+    private Double x = -2.58;
+    private Double y = 0.23;
+    private String host = "192.168.199.124";
+    private String user = "turtlebot";
+    private Integer port = 22;
+    private String psw = "1012";
+    private String command = "python /helloworld/turtlebot/go_to_specific_point_on_map.py " + x + " " + y;
 
     @Override
     public Integer orderDispatch(Indent indent) {
 
-        return null;
+        String res = new SSHshell(host,user,psw,port,command).exec();
+        System.out.println(res);
+        return 1;
     }
 }
